392973f4db474ee06514815b538ada591d958ea1
Marcin Kwiatkowski <contact@frodigo.com>
2020-02-06 20:04:50 +0100

    Added base module files and db_schema for catalog_banners table

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @copyright Copyright (C) 2020 Marcin Kwiatkowski (http://marcin-kwiatkowski.com)
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mkwiatkowski_CatalogBanners', __DIR__);
